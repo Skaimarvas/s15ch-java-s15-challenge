11cf2c4149ad7480cf9c01c8e175038fa4929515
skaimarvas <skaimarvas@gmail.com>
2024-01-27 12:20:33 +0300

    Status(enum) oluşturuldu.

---
+++ src/Status.java
@@ -0,0 +1,15 @@
+public enum Status {
+    AVAILABLE("Available in the library"),
+    ON_LOAN("Borrowed"),
+    NOT_IN_CATALOG("Not available in the library records");
+
+    private final String description;
+
+    Status(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
